bf6df28bf9d7f9126bf5142a8c04107aaa701fce
Saqib <saqibnaseeb14@gmail.com>
2022-09-12 02:17:52 +0500

    restructuring of HTML + multiple instance of component + transition effects

--- component.js
+++ component.js
@@ -1,66 +1,109 @@
-// Some global varaibles being used in multiple functions
-let slideIndex = 0;
-let slides = document.getElementsByClassName("slide");
-let dots = document.getElementsByClassName("dot");
-  
-  /*
-  * Initialise and render the initial slides and configure to autoplay every 3 seconds
-  */
-
-  const showSlides=()=> {    
-    if (slideIndex >= slides.length) {slideIndex = 0}    
-    if (slideIndex < 0) {slideIndex = slides.length - 1}
-    currentSlide();
-    slideIndex++;
-    setTimeout(showSlides, 3000); // Change slide every 3 seconds
-}
-
-  /*
-  * Event Listener for slide change when clicked on arrows
-  */ 
-  const changeSlide =(slideToIndex)=>{
-       slideIndex = slideIndex + slideToIndex;
-       if (slideIndex >= slides.length) {slideIndex = 0}    
-       if (slideIndex < 0) {slideIndex = slides.length - 1}
-       currentSlide();
+class SlideComponent {
+  // Some class attributes required in Slider
+  constructor(sliderContainer,userContentList) {
+    this.sliderContainer = sliderContainer;
+    this.arrowCreateRenderer();
+    this.slideIndex = 0;
+    this.slides = this.sliderContainer.getElementsByClassName("slide");
+    this.dots = this.sliderContainer.getElementsByClassName("dot");
+    this.init(userContentList);
   }
 
-  /*
-  * Get the current slide set and make the dot active
-  */ 
-  const currentSlide=()=>{ 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-        dots[i].classList.remove("current_active");
-    }
-    slides[slideIndex].style.display = "block";
-    dots[slideIndex].classList.add("current_active");
-  }
-
-  /*
-   * Dots renderer to highlight the current active slide as well as quick access 
-   */
-  const dotsRenderer=(slider,options)=>{
-    for(let t=0;t<options.noOfDots;t++){
-      const dotElement = document.createElement('div');
-      dotElement.classList.add("dot");
-      dotElement.onclick= ()=> {slideIndex = t;currentSlide();};
-      slider?.appendChild(dotElement);
-  }
-  }
-
-  /*
-  * Content Component to be rendered in the slider
-  */
-  const contentComponent=(userContentList)=>{
-    const slider = document.querySelector(".slide_content");
+  init(userContentList){
+    const sliderContent = this.sliderContainer.querySelector('.slide_content');
+    const sliderWrapper= document.createElement('div');
     userContentList.forEach(content => {
       let element = document.createElement('div');
       element.classList.add('slide');
       element.classList.add('fade_effect');
       element.innerHTML = content;
-      slider?.appendChild(element);  
+      sliderWrapper?.appendChild(element);  
     });
-    dotsRenderer(slider,{noOfDots: userContentList.length});
-    showSlides();
+    sliderContent.appendChild(sliderWrapper);
+    this.dotsRenderer(sliderContent,{noOfDots: userContentList.length});
+    this.showSlides();
+  }
+
+  arrowCreateRenderer =()=>{
+    const  leftArrowImg = document.createElement('img');
+    leftArrowImg.setAttribute(
+      'src',
+      './assets/arrow.svg',
+    );
+    leftArrowImg.classList.add("slider_arrow");
+    leftArrowImg.classList.add("left_arrow");
+    leftArrowImg.addEventListener('click',()=>this.changeSlide(-1));
+
+    const sliderInnerContent =  document.createElement('div');
+    sliderInnerContent.classList.add("slide_content");
+
+    const rightArrowImg =  document.createElement('img');
+    rightArrowImg.classList.add("slider_arrow");
+    rightArrowImg.addEventListener('click',()=>this.changeSlide(1));
+    rightArrowImg.setAttribute(
+      'src',
+      './assets/arrow.svg',
+    );
+   
+    this.sliderContainer.appendChild(leftArrowImg);
+    
+    this.sliderContainer.appendChild(sliderInnerContent);
+  
+    this.sliderContainer.appendChild(rightArrowImg);
+  }
+    /*
+  * Initialise and render the initial slides and configure to autoplay every 3 seconds
+  */
+  showSlides=()=> {    
+    if (this.slideIndex >= this.slides.length) {this.slideIndex = 0}    
+    if (this.slideIndex < 0) {this.slideIndex = this.slides.length - 1}
+    this.currentSlide();
+    this.slideIndex++;
+    setTimeout(this.showSlides, 3000); // Change slide every 3 seconds
+  }
+
+    /*
+  * Event Listener for slide change when clicked on arrows
+  */ 
+  changeSlide =(slideToIndex)=>{
+      this.slideIndex = this.slideIndex + slideToIndex;
+      if (this.slideIndex >= this.slides.length) {this.slideIndex = 0}    
+      if (this.slideIndex < 0) {this.slideIndex = this.slides.length - 1}
+      this.currentSlide();
+ }
+
+   /*
+  * Get the current slide set and make the dot active
+  */ 
+  currentSlide=()=>{ 
+    for (let i = 0; i < this.slides.length; i++) {
+      this.slides[i].style.display = "none"; 
+      this.dots[i].classList.remove("current_active");
+    }
+    this.slides[this.slideIndex].style.display = "block";
+    this.dots[this.slideIndex].classList.add("current_active");
+  }
+
+
+    /*
+   * Dots renderer to highlight the current active slide as well as quick access 
+   */
+  dotsRenderer=(sliderContent,options)=>{
+    for(let t=0;t<options.noOfDots;t++){
+      const dotElement = document.createElement('div');
+      dotElement.classList.add("dot");
+      dotElement.onclick= ()=> {this.slideIndex = t;this.currentSlide();};
+      sliderContent?.appendChild(dotElement);
+  }
+  }
+}
+
+  /*
+  * Content Component to be rendered in the this.sliderContainer
+  */
+  const contentComponent=(userContentList)=>{
+    const sliders = document.querySelectorAll(".slider");
+    sliders?.forEach((sliderContainer)=>{
+      new SlideComponent(sliderContainer,userContentList); 
+    })
   }
